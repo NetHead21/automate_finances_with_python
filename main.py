db9ba06045bbbfbb7502bacf985de01440d63ad8
nethead21 <junivensaavedra@gmail.com>
2025-05-16 15:59:51 +0800

    Added load_transaction function

--- main.py
+++ main.py
@@ -7,10 +7,26 @@ import os
 st.set_page_config(page_title="Simple Finance App", page_icon="$", layout="wide")
 
 
+def load_transactions(file: str): 
+    try:
+        df = pd.read_csv(file)
+        df.columns = [col.strip() for col in df.columns]
+        df["Amount"] = df["Amount"].str.replace(",", "").astype(float)
+        df["Date"] = pd.to_datetime(df["Date"], format="%d %b %Y")
+        st.write(df)
+        return df
+    except Exception as e:
+        st.error(f"Error processing file {e}")
+        return None
+
+
 def main():
     st.title("Simple Finance Dashboard")
 
     uploaded_file = st.file_uploader("Upload your transaction CSV file", type=["csv"])
 
-    if uploaded_file is not NOne:
+    if uploaded_file is not None:
         df = load_transactions(uploaded_file)
+
+
+main()
